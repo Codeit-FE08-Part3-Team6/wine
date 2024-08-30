3be722888de41da87ae1ea4ef8874a0008ab9328
leehj322 <gudwns8956@naver.com>
2024-08-31 04:44:06 +0900

    feat: GlobaNavBar 세부 구현 완료
    - 드롭다운 미적용

--- src/components/@shared/GlobalNavBar.tsx
+++ src/components/@shared/GlobalNavBar.tsx
@@ -1,9 +1,79 @@
 import Image from "next/image";
 import { useRouter } from "next/router";
+import { useState, useEffect } from "react";
+import getUserProfile from "@/libs/axios/user/getUserProfile";
+import Link from "next/link";
+import { removeTokens } from "@/utils/authTokenStorage";
 
+interface ProfileImgProps {
+  profileImg: string | null;
+  setIsLogin: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+function ProfileImg({ profileImg, setIsLogin }: ProfileImgProps) {
+  const handleLogoutBtnClick = () => {
+    removeTokens();
+    setIsLogin(false);
+  };
+
+  // 드롭다운 공통 컴포넌트 완성되면 적용 할 예정
+  return (
+    <div>
+      <Image
+        className="block md:hidden"
+        width={30}
+        height={30}
+        src={profileImg ?? "/images/img_pfp_default.svg"}
+        alt="프로필 이미지"
+      />
+      <Image
+        className="hidden md:block"
+        width={40}
+        height={40}
+        src={profileImg ?? "/images/img_pfp_default.svg"}
+        alt="프로필 이미지"
+      />
+      <div className="absolute z-50 w-[100px] text-gray-500">
+        <Link href="/myprofile">마이페이지</Link>
+        <button type="button" onClick={handleLogoutBtnClick}>
+          로그아웃
+        </button>
+      </div>
+    </div>
+  );
+}
+
+/**
+ * Navigation Bar
+ * width 기본 값이 full 이므로, wrapper 사용!
+ */
 export default function GlobalNavBar() {
   const { pathname } = useRouter();
-  const logoImg = undefined;
+  const [isLogin, setIsLogin] = useState(false);
+  const [profileImg, setProfileImg] = useState<string | null>(null);
+
+  // 로그인 상태인 경우 프로필 이미지 받아오기
+  useEffect(() => {
+    if (localStorage.getItem("accessToken")) {
+      setIsLogin(true);
+
+      const loadUserProfileImage = async () => {
+        try {
+          const user = await getUserProfile();
+          if (user?.image) {
+            setProfileImg(user.image);
+          }
+        } catch (error) {
+          console.error(error);
+        }
+      };
+
+      // eslint-disable-next-line
+      loadUserProfileImage(); // Promise must be awaited (@typescript-eslint/no-floating-promises)
+    } else {
+      // setIsLogin(false);
+    }
+  }, [isLogin]);
 
   return (
     <header className="flex h-[50px] w-full items-center justify-between rounded-2xl bg-light-black px-5 text-md-14px-medium text-light-white md:h-[70px] md:px-[60px] md:text-lg-16px-medium">
@@ -14,27 +84,12 @@ export default function GlobalNavBar() {
         alt="로고 이미지"
       />
       <div className="flex gap-5 md:gap-10">
-        {localStorage.getItem("accessToken") ? (
-          <>
-            <Image
-              className="block md:hidden"
-              width={30}
-              height={30}
-              src={logoImg ?? "/images/img_pfp_default.svg"}
-              alt="프로필 이미지"
-            />
-            <Image
-              className="hidden md:block"
-              width={40}
-              height={40}
-              src={logoImg ?? "/images/img_pfp_default.svg"}
-              alt="프로필 이미지"
-            />
-          </>
+        {isLogin ? (
+          <ProfileImg profileImg={profileImg} setIsLogin={setIsLogin} />
         ) : (
           <>
-            <div>로그인</div>
-            {pathname === "/" && <div>회원가입</div>}
+            <Link href="/signin">로그인</Link>
+            {pathname === "/" && <Link href="/signup">회원가입</Link>}
           </>
         )}
       </div>

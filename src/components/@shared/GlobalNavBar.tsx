a25944b07b2766a5f284f22adbb6bef2d4e5cfe7
leehj322 <gudwns8956@naver.com>
2024-08-30 19:38:11 +0900

    feat: GlobalNavBar UI 구현
    - 프로필 이미지 가져오기 위한 API 호출 제외 구현 완료

--- src/components/@shared/GlobalNavBar.tsx
+++ src/components/@shared/GlobalNavBar.tsx
@@ -1,3 +1,43 @@
+import Image from "next/image";
+import { useRouter } from "next/router";
+
 export default function GlobalNavBar() {
-  return <div>네비게이션바</div>;
+  const { pathname } = useRouter();
+  const logoImg = undefined;
+
+  return (
+    <header className="flex h-[50px] w-full items-center justify-between rounded-2xl bg-light-black px-5 text-md-14px-medium text-light-white md:h-[70px] md:px-[60px] md:text-lg-16px-medium">
+      <Image
+        width={52}
+        height={15}
+        src="/images/logo_white.png"
+        alt="로고 이미지"
+      />
+      <div className="flex gap-5 md:gap-10">
+        {localStorage.getItem("accessToken") ? (
+          <>
+            <Image
+              className="block md:hidden"
+              width={30}
+              height={30}
+              src={logoImg ?? "/images/img_pfp_default.svg"}
+              alt="프로필 이미지"
+            />
+            <Image
+              className="hidden md:block"
+              width={40}
+              height={40}
+              src={logoImg ?? "/images/img_pfp_default.svg"}
+              alt="프로필 이미지"
+            />
+          </>
+        ) : (
+          <>
+            <div>로그인</div>
+            {pathname === "/" && <div>회원가입</div>}
+          </>
+        )}
+      </div>
+    </header>
+  );
 }

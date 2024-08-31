b8964330af1b7f5164f12a90fbfe706e28390322
Jaeheon96 <jinnavsi015@naver.com>
2024-08-31 21:45:05 +0900

    feat: 텍스트 input과 선택지 input ui 구현

--- src/pages/signin/index.tsx
+++ src/pages/signin/index.tsx
@@ -1,3 +1,45 @@
+import Input from "@/components/@shared/Input";
+import InputSelect from "@/components/@shared/InputSelect";
+import { useState } from "react";
+
 export default function SignInPage() {
-  return <div>로그인 페이지</div>;
+  const [wineType, setWineType] = useState<"Red" | "White" | "Sparkling">(
+    "Red",
+  );
+  const [isButtonsVisible, setIsButtonsVisible] = useState(false);
+
+  // return <div>로그인 페이지</div>;
+  return (
+    <>
+      <Input placeholder="테스트" />
+      <InputSelect
+        placeholder={wineType}
+        onClick={() => {
+          setIsButtonsVisible(!isButtonsVisible);
+        }}
+      />
+      {isButtonsVisible && (
+        <>
+          <button
+            className="border border-solid p-2"
+            onClick={() => setWineType("Red")}
+          >
+            Red
+          </button>
+          <button
+            className="border border-solid p-2"
+            onClick={() => setWineType("White")}
+          >
+            White
+          </button>
+          <button
+            className="border border-solid p-2"
+            onClick={() => setWineType("Sparkling")}
+          >
+            Sparkling
+          </button>
+        </>
+      )}
+    </>
+  );
 }

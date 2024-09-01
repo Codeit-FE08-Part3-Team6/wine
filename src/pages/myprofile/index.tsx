e6228b40ddc650fb600eba33f4adc2862814541c
hyeoksuJ <hyeoksu459@naver.com>
2024-09-01 22:14:35 +0900

    refactor: 테스트용 코드 지움

--- src/pages/myprofile/index.tsx
+++ src/pages/myprofile/index.tsx
@@ -1,15 +1,3 @@
-import FlavorRangeInput from "@/components/@shared/FlavorRangeInput";
-import PriceRangeInput from "@/components/@shared/PriceRangeInput";
-import TextareaInput from "@/components/@shared/TextareaInput";
-
 export default function MyProfilePage() {
-  return (
-    <div className="w-[500px] px-[50px] py-[50px]">
-      <PriceRangeInput />
-      <div className="py-[20px]" />
-      <FlavorRangeInput min={0} max={10} />
-      <div className="py-[20px]" />
-      <TextareaInput />
-    </div>
-  );
+  return <div>마이 프로필 페이지</div>;
 }

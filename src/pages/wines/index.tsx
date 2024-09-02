3742e33ea213fa4e5205b5fec9daf8bee38afcae
Injaeeee <97098671+Injaeeee@users.noreply.github.com>
2024-09-02 15:15:04 +0900

    refactor : lint 설정 추가

--- src/pages/wines/index.tsx
+++ src/pages/wines/index.tsx
@@ -1,7 +1,7 @@
 import WineItemList from "@/components/wines/WineItemList";
-import { getWineRecommends } from "@/libs/axios/wine/getWineRecommends";
-import { getWines } from "@/libs/axios/wine/getWines";
-import { postWines } from "@/libs/axios/wine/postWines";
+import getWineRecommends from "@/libs/axios/wine/getWineRecommends";
+import getWines from "@/libs/axios/wine/getWines";
+import postWines from "@/libs/axios/wine/postWines";
 
 import { PostWineDetails, Wine, WineEnum } from "@/types/wines";
 import { useEffect, useState } from "react";
@@ -19,28 +19,39 @@ export default function WineListPage() {
   });
 
   async function fetchWines() {
-    const wineList = await getWines(); //와인 목록 조회
-    const wineRecommendList = await getWineRecommends(); //추천 와인 목록 조회
-    setWineList(wineList);
+    const getWineList: Wine[] = await getWines(); // 와인 목록 조회
+    const wineRecommendList: Wine[] = await getWineRecommends(); // 추천 와인 목록 조회
+    setWineList(getWineList);
+    console.log(wineRecommendList);
   }
 
   useEffect(() => {
-    fetchWines();
+    fetchWines()
+      .then(() => {
+        // 성공적으로 데이터 로드
+      })
+      .catch((error) => {
+        console.error("Error during fetching data:", error);
+      });
   }, []);
 
-  const handleSubmit = async (e: React.MouseEvent<HTMLButtonElement>) => {
-    e.preventDefault();
-    const result = await postWines(wineValue);
-    if (!result) {
-      console.log("wine 등록 중 오류 발생");
-    }
-  };
+  // const handleSubmit = async (e: React.MouseEvent<HTMLButtonElement>) => {
+  //   e.preventDefault();
+  //   try {
+  //       const result = await postWines(wineValue);
+  //       if (!result) {
+  //           console.log("wine 등록 중 오류 발생");
+  //       }
+  //   } catch (error) {
+  //       console.error("비동기 작업 중 오류 발생:", error);
+  //   }
+  // };
 
   return (
     <div className="flex w-[1920px] justify-center">
       <div className="max-w-[1140px]">
         <div>이번달 추천 와인</div>
-        <button onClick={handleSubmit}>wine 등록</button>
+        {/* <button type="button" onClick={handleSubmit}>wine 등록</button> */}
         <div className="flex">
           <div className="w-[284px]">WineTypes</div>
           <WineItemList wines={wineList} />

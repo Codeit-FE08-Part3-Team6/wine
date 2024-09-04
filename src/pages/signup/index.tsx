6c9c555e5500bbb828e669890749b99659666658
Jaeheon96 <97386020+Jaeheon96@users.noreply.github.com>
2024-09-04 14:25:15 +0900

    [TM-55] Feat(손재헌): 로그인 회원가입 UI 구현 (#36)
    * feat: 유저기능(로그인, 로그아웃, 유저정보 등) 컨텍스트 구현
    
    * refactor: 유저 컨텍스트 useEffect의 getMe가 유저데이터가 없을때만
    호출되도록 수정
    
    * feat: 유저기능 컨텍스트 구현/eslint 오류 해결
    
    * refactor: 로그인 상태에서 로그인, 회원가입 페이지 접근시 마이페이지로
    리다이렉트 하도록 수정
    
    * refactor: api 가 axios.catch를 활용하도록 리팩토링, 컨텍스트 테스트 후
    수정
    
    * feat: 로그인, 회원가입 페이지 UI 구현

--- src/pages/signup/index.tsx
+++ src/pages/signup/index.tsx
@@ -2,6 +2,12 @@ import { ChangeEvent, FormEvent, useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import { signUp } from "@/libs/axios/auth/auth";
 import { useAuth } from "@/contexts/AuthProvider";
+import AuthLabel from "@/components/auth/AuthLabel";
+import Link from "next/link";
+import Image from "next/image";
+import Input from "@/components/@shared/Input";
+import InputPassword from "@/components/@shared/InputPassword";
+import Button from "@/components/@shared/Button";
 
 export default function SignUpPage() {
   const [formData, setFormData] = useState({
@@ -35,40 +41,68 @@ export default function SignUpPage() {
   }, [user]);
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        <p>이메일</p>
-        <input
-          name="email"
-          className="border border-solid"
-          onChange={handleInputChange}
-        />
-      </label>
-      <label>
-        <p>닉네임</p>
-        <input
-          name="nickname"
-          className="border border-solid"
-          onChange={handleInputChange}
-        />
-      </label>
-      <label>
-        <p>비밀번호</p>
-        <input
-          name="password"
-          className="border border-solid"
-          onChange={handleInputChange}
-        />
-      </label>
-      <label>
-        <p>비밀번호 확인</p>
-        <input
-          name="passwordConfirmation"
-          className="border border-solid"
-          onChange={handleInputChange}
-        />
-      </label>
-      <button type="submit">회원가입</button>
-    </form>
+    <main className="flex h-screen w-full items-center justify-center bg-gray-100 px-4">
+      <form
+        className="flex w-[496px] flex-col items-center rounded-2xl border border-solid border-gray-300 bg-white px-5 py-14 md:px-12 md:py-16 xl:py-20"
+        onSubmit={handleSubmit}
+      >
+        <Link href="/">
+          <Image
+            src="/images/logo_black.png"
+            width={104}
+            height={30}
+            alt="홈으로"
+          />
+        </Link>
+        <AuthLabel
+          className="mb-2.5 mt-14 w-full md:mb-6 md:mt-16"
+          label="이메일"
+        >
+          <Input
+            name="email"
+            placeholder="whyne@email.com"
+            onChange={handleInputChange}
+            value={formData.email}
+          />
+        </AuthLabel>
+        <AuthLabel className="mb-2.5 w-full md:mb-6" label="닉네임">
+          <Input
+            name="nickname"
+            placeholder="whyne"
+            onChange={handleInputChange}
+            value={formData.nickname}
+          />
+        </AuthLabel>
+        <AuthLabel className="mb-2.5 w-full md:mb-6" label="비밀번호">
+          <InputPassword
+            name="password"
+            placeholder="영문, 숫자 포함 8자 이상"
+            onChange={handleInputChange}
+            value={formData.password}
+          />
+        </AuthLabel>
+        <AuthLabel className="mb-10 w-full md:mb-8" label="비밀번호 확인">
+          <InputPassword
+            name="passwordConfirmation"
+            placeholder="비밀번호 확인"
+            onChange={handleInputChange}
+            value={formData.passwordConfirmation}
+          />
+        </AuthLabel>
+        <Button style={{ height: "48px" }} buttonStyle="purple" type="submit">
+          회원가입
+        </Button>
+        <div className="mt-10 flex gap-3.5">
+          <span className="text-md-14px-regular text-gray-500 md:text-lg-16px-regular">
+            계정이 이미 있으신가요?
+          </span>
+          <Link href="/signin">
+            <span className="text-md-14px-medium text-light-purple-100 underline md:text-lg-16px-medium">
+              로그인하기
+            </span>
+          </Link>
+        </div>
+      </form>
+    </main>
   );
 }

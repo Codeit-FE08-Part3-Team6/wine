85ad6a40354f944cc4c5652d84dd77699ce141ee
leehj322 <gudwns8956@naver.com>
2024-09-04 14:26:45 +0900

    feat: landing page head 추가
    - meta tag 추가
    - title 추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,21 +4,46 @@ import Button from "@/components/@shared/Button";
 import HeroImg from "@/components/landing/HeroImg";
 import MobileMain from "@/components/landing/MobileMain";
 import DefaultMain from "@/components/landing/DefaultMain";
+import Head from "next/head";
 
 export default function HomePage() {
   return (
-    <div className="bg-light-default pb-20 pt-6">
-      <header className="mx-4 max-w-[1140px] md:mx-5 xl:mx-auto">
-        <GlobalNavBar />
-      </header>
-      <main className="mx-4 mt-6 max-w-[1140px] md:mx-5 xl:mx-auto xl:mb-[104px] xl:mt-20">
-        <HeroImg />
-        <DefaultMain />
-        <MobileMain />
-        <Link href="/wines" className="mx-auto block h-[50px] w-[279px]">
-          <Button buttonStyle="purple">와인 보러가기</Button>
-        </Link>
-      </main>
-    </div>
+    <>
+      <Head>
+        <title>WINE</title>
+
+        <meta property="og:locale" content="ko_KR" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://wine-nine-xi.vercel.app/" />
+        <meta property="og:site_name" content="WINE" />
+        <meta property="og:title" content="WINE - 나만의 와인창고" />
+        <meta
+          property="og:description"
+          content="한 곳에서 관리하는 나만의 와인창고"
+        />
+        <meta property="og:image" content="/images/landing/lg_main_hero.png" />
+
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:title" content="WINE - 나만의 와인창고" />
+        <meta
+          name="twitter:description"
+          content="한 곳에서 관리하는 나만의 와인창고"
+        />
+        <meta name="twitter:image" content="/images/landing/lg_main_hero.png" />
+      </Head>
+      <div className="bg-light-default pb-20 pt-6">
+        <header className="mx-4 max-w-[1140px] md:mx-5 xl:mx-auto">
+          <GlobalNavBar />
+        </header>
+        <main className="mx-4 mt-6 max-w-[1140px] md:mx-5 xl:mx-auto xl:mb-[104px] xl:mt-20">
+          <HeroImg />
+          <DefaultMain />
+          <MobileMain />
+          <Link href="/wines" className="mx-auto block h-[50px] w-[279px]">
+            <Button buttonStyle="purple">와인 보러가기</Button>
+          </Link>
+        </main>
+      </div>
+    </>
   );
 }

eb3b368e0c8516c9c26e71931505b35f22f95f24
leehj322 <gudwns8956@naver.com>
2024-09-04 19:40:38 +0900

    refactor: 프로필 이미지 위치 올바르게 수정 및 eslint 무시 해제

--- src/components/@shared/GlobalNavBar.tsx
+++ src/components/@shared/GlobalNavBar.tsx
@@ -23,14 +23,14 @@ function ProfileImg({ profileImg, setIsLogin }: ProfileImgProps) {
       buttonChildren={
         <>
           <Image
-            className="block md:hidden"
+            className="inline-block md:hidden"
             width={30}
             height={30}
             src={profileImg ?? "/images/img_pfp_default.svg"}
             alt="프로필 이미지"
           />
           <Image
-            className="hidden md:block"
+            className="hidden md:inline-block"
             width={40}
             height={40}
             src={profileImg ?? "/images/img_pfp_default.svg"}
@@ -72,15 +72,14 @@ export default function GlobalNavBar() {
         }
       };
 
-      // eslint-disable-next-line
-      loadUserProfileImage(); // Promise must be awaited (@typescript-eslint/no-floating-promises)
+      loadUserProfileImage();
     } else {
       setIsLogin(false);
     }
   }, [isLogin]);
 
   return (
-    <header className="flex h-[50px] w-full items-center justify-between rounded-2xl bg-light-black px-5 text-md-14px-medium text-light-white md:h-[70px] md:px-[60px] md:text-lg-16px-medium">
+    <div className="flex h-[50px] w-full items-center justify-between rounded-2xl bg-light-black px-5 text-md-14px-medium text-light-white md:h-[70px] md:px-[60px] md:text-lg-16px-medium">
       <Link href="/">
         <Image
           width={52}
@@ -99,6 +98,6 @@ export default function GlobalNavBar() {
           </>
         )}
       </div>
-    </header>
+    </div>
   );
 }

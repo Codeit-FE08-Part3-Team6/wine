3f445a1ed03145a47cdbc634e2ceb0248f0a3c54
Injaeeee <97098671+Injaeeee@users.noreply.github.com>
2024-09-05 16:12:40 +0900

    feat : 반응형 모바일 일부 구현
    모바일 환경일 때 필터, 등록 버튼은 아직 미구현 상태입니다

--- src/components/wines/WineItemList.tsx
+++ src/components/wines/WineItemList.tsx
@@ -13,14 +13,14 @@ interface WineProps {
 function WineItemCard({ wine }: WineProps) {
   return (
     <div className="flex h-[375px] w-full flex-col rounded-2xl border border-light-gray-300 bg-light-white">
-      <div className="flex h-2/3 flex-row border-b">
-        <div className="w-1/5">
-          이미지 영역
+      <div className="flex h-2/3 flex-row border-b px-12 max-[375px]:px-3">
+        <div className="flex w-1/6 items-end pr-5 max-[375px]:w-1/3">
+          <div className="h-4/5"> 이미지 영역 </div>
           {/* {wine.image && <Image src={wine.image} alt="와인이미지" />} */}
         </div>
-        <div className="flex w-4/5 flex-row justify-between py-9 pr-14">
+        <div className="flex w-4/5 flex-row justify-between py-9 max-[375px]:w-2/3 max-[375px]:flex-col max-[375px]:gap-5 max-[375px]:py-6">
           <div className="flex flex-col gap-2">
-            <p className="text-3xl-32px-semibold text-light-gray-800">
+            <p className="text-3xl-32px-semibold text-light-gray-800 max-[375px]:text-xl-20px-semibold">
               {wine.name}
             </p>
             <p className="text-lg-16px-regular text-light-gray-500">
@@ -32,8 +32,8 @@ function WineItemCard({ wine }: WineProps) {
               </p>
             </div>
           </div>
-          <div className="flex flex-col justify-between">
-            <div>
+          <div className="flex flex-col justify-between max-[375px]:flex-row">
+            <div className="max-[375px]:flex max-[375px]:gap-5">
               <p className="text-3xl-32px-semibold text-light-gray-800">4.8</p>
               <p className="text-lg-16px-regular text-light-gray-500">
                 {wine.reviewerCount ? `${wine.reviewerCount}` : 0}개의 후기
@@ -51,7 +51,7 @@ function WineItemCard({ wine }: WineProps) {
         </div>
       </div>
 
-      <div className="flex flex-col gap-1 p-5">
+      <div className="flex flex-col gap-1 p-5 max-[375px]:pt-2">
         <p className="text-lg-16px-semibold text-light-gray-800">최신후기</p>
         <p className="text-lg-16px-regular text-light-gray-500">
           Cherry, cocoa, vanilla and clove - beautiful red fruit driven Low
@@ -65,7 +65,7 @@ function WineItemCard({ wine }: WineProps) {
 
 export default function WineItemList({ wines }: WinesProps) {
   return (
-    <div className="flex max-w-[800px] flex-col gap-16">
+    <div className="flex w-full flex-col gap-16">
       {wines.map((wine) => (
         <Link key={wine.id} href={`/wines/${wine.id.toString()}`}>
           <WineItemCard wine={wine} />

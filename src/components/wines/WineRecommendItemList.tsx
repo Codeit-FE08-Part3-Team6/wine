0917a9d3acb381630de29a628a0880e62bd94084
Injaeeee <97098671+Injaeeee@users.noreply.github.com>
2024-09-07 00:49:08 +0900

    feat : 반응형 폰트 변경

--- src/components/wines/WineRecommendItemList.tsx
+++ src/components/wines/WineRecommendItemList.tsx
@@ -14,7 +14,7 @@ function WineRecommendCard({ wine }: WineProps) {
     <div className="box-border flex h-[185px] w-[232px] shrink-0 rounded-2xl bg-light-white px-6 pt-6 max-md:w-[193px]">
       <div className="w-2/5">이미지</div>
       <div className="flex w-3/5 flex-col gap-2">
-        <p className="text-4xl">
+        <p className="text-4xl font-extrabold">
           {wine.avgRating ? wine.avgRating.toFixed(1) : 0}
         </p>
         <Rating
@@ -34,17 +34,39 @@ export default function WineRecommendItemList() {
   const [currentScrollValue, setCurrentScrollValue] = useState(0);
   const [scrollRange, setScrollRange] = useState(900);
   const [wineRecommends, setWineRecommends] = useState<Wine[]>([]);
+  const [hasWineButtons, setHasWineButtons] = useState({
+    isRightBtnVisible: false,
+    isLeftBtnVisible: false,
+  });
 
   async function fetchWines() {
     const wineRecommendList: Wine[] = await getWineRecommends(); // 추천 와인 목록 조회
     setWineRecommends(wineRecommendList);
   }
 
+  function buttonCheck() {
+    if (!containerRef.current) {
+      return;
+    }
+    const isRightBtnVisible =
+      containerRef.current.scrollWidth >
+        containerRef.current.clientWidth + currentScrollValue + 1 &&
+      containerRef.current.clientWidth < 48 + 244 * wineRecommends.length - 20;
+
+    const isLeftBtnVisible = currentScrollValue > 0;
+
+    console.log(
+      wineRecommends.length,
+      containerRef.current.clientWidth,
+      48 + 244 * wineRecommends.length - 20,
+      containerRef.current.scrollLeft,
+    );
+    setHasWineButtons({ isRightBtnVisible, isLeftBtnVisible });
+  }
+
   useEffect(() => {
     fetchWines()
-      .then(() => {
-        // 성공적으로 데이터 로드
-      })
+      .then(() => {})
       .catch((error) => {
         console.error("Error during fetching data:", error);
       });
@@ -57,6 +79,15 @@ export default function WineRecommendItemList() {
     const currentRef = containerRef.current;
     currentRef?.addEventListener("scroll", handleScroll);
   }, []);
+
+  useEffect(() => {
+    buttonCheck();
+
+    window.addEventListener("resize", buttonCheck);
+    return () => {
+      window.removeEventListener("resize", buttonCheck);
+    };
+  }, [currentScrollValue, wineRecommends]);
 
   useEffect(() => {
     const handleResize = () => {
@@ -91,13 +122,13 @@ export default function WineRecommendItemList() {
   };
 
   return (
-    <div className="flex h-[299px] max-w-[1140px] flex-col rounded-2xl bg-light-gray-100 max-xl:w-[704px] max-md:w-[343px]">
+    <div className="flex h-[299px] w-full flex-col rounded-2xl bg-light-gray-100">
       <div className="flex flex-col gap-6 py-4">
         <span className="px-6 text-xl-20px-bold text-light-gray-800">
           이번 달 추천 와인
         </span>
         <div className="relative">
-          {currentScrollValue > 0 && (
+          {hasWineButtons.isLeftBtnVisible && (
             <button
               type="button"
               className="absolute left-4 top-1/2 flex h-[48px] w-[48px] -translate-y-1/2 items-center justify-center rounded-full border border-solid border-light-gray-300 bg-light-white"
@@ -119,26 +150,23 @@ export default function WineRecommendItemList() {
             style={{ scrollBehavior: "smooth" }}
           >
             {wineRecommends.map((wine) => (
-              // eslint-disable-next-line react/no-array-index-key
               <WineRecommendCard key={wine.id} wine={wine} />
             ))}
           </div>
-          {containerRef.current &&
-            containerRef.current.scrollWidth >
-              containerRef.current.clientWidth + currentScrollValue + 1 && (
-              <button
-                type="button"
-                className="absolute right-4 top-1/2 flex h-[48px] w-[48px] -translate-y-1/2 items-center justify-center rounded-full border border-solid border-light-gray-300 bg-light-white"
-                onClick={() => handleClick("right")}
-              >
-                <Image
-                  src="/images/ic_x_scroll_right.svg"
-                  alt="arrowIcon"
-                  width={16}
-                  height={16}
-                />
-              </button>
-            )}
+          {hasWineButtons.isRightBtnVisible && (
+            <button
+              type="button"
+              className="absolute right-4 top-1/2 flex h-[48px] w-[48px] -translate-y-1/2 items-center justify-center rounded-full border border-solid border-light-gray-300 bg-light-white"
+              onClick={() => handleClick("right")}
+            >
+              <Image
+                src="/images/ic_x_scroll_right.svg"
+                alt="arrowIcon"
+                width={16}
+                height={16}
+              />
+            </button>
+          )}
         </div>
       </div>
     </div>

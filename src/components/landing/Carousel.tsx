fce14e327475b8c331f8c5c3b8b91369705b187b
leehj322 <gudwns8956@naver.com>
2024-09-10 21:04:56 +0900

    feat: 좌측 우측 넘어가는 버튼 추가
    - 이미지 왼쪽 혹은 오른쪽 클릭시 이전, 다음 페이지로 이동
    - 아래쪽 slide indicator 추가
    - slide indicator onClick 추가

--- src/components/landing/Carousel.tsx
+++ src/components/landing/Carousel.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode, useState, useEffect } from "react";
+import React, { ReactNode, useState } from "react";
 
 interface CarouselProps {
   children: ReactNode;
@@ -8,11 +8,23 @@ export default function Carousel({ children }: CarouselProps) {
   const [currentSlide, setCurrentSlide] = useState(0);
   const slidesLength = React.Children.count(children);
 
-  useEffect(() => {
-    setCurrentSlide(0);
-    console.log(currentSlide);
-    console.log(slidesLength);
-  }, []);
+  const handleRightBtnClick = () => {
+    if (currentSlide === slidesLength - 1) {
+      setCurrentSlide(0);
+      return;
+    }
+
+    setCurrentSlide((prevCurrentSlide) => prevCurrentSlide + 1);
+  };
+
+  const handleLeftBtnClick = () => {
+    if (currentSlide === 0) {
+      setCurrentSlide(slidesLength - 1);
+      return;
+    }
+
+    setCurrentSlide((prevCurrentSlide) => prevCurrentSlide - 1);
+  };
 
   return (
     <div className="relative h-full w-full overflow-hidden">
@@ -21,6 +33,32 @@ export default function Carousel({ children }: CarouselProps) {
         style={{ transform: `translateX(-${currentSlide * 100}%)` }}
       >
         {children}
+      </div>
+      <button
+        type="button"
+        className="absolute bottom-0 left-0 top-0 w-[50%]"
+        onClick={handleLeftBtnClick}
+        aria-label="slide-left-button"
+      />
+      <button
+        type="button"
+        className="absolute bottom-0 right-0 top-0 w-[50%]"
+        onClick={handleRightBtnClick}
+        aria-label="slide-right-button"
+      />
+      <div className="absolute bottom-0 left-[50%] flex translate-x-[-50%] gap-2">
+        {Array.from({ length: 3 }, (_, i) => i).map((idx) => (
+          <button
+            key={idx}
+            type="button"
+            className="h-[10px] w-[50px] rounded-full transition-colors duration-300"
+            style={{
+              backgroundColor: currentSlide === idx ? "#cfdbea" : "#ffffff",
+            }}
+            aria-label="slide-indicator-button"
+            onClick={() => setCurrentSlide(idx)}
+          />
+        ))}
       </div>
     </div>
   );

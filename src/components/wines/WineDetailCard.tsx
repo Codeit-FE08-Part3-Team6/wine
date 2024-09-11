26b0a407e96d6ced7d616e431b44881f90536982
KJongHwa <100202720+KJongHwa@users.noreply.github.com>
2024-09-11 17:55:03 +0900

    Refector : api 1번만 조회하게 수정

--- src/components/wines/WineDetailCard.tsx
+++ src/components/wines/WineDetailCard.tsx
@@ -1,38 +1,11 @@
-import { useEffect, useState } from "react";
 import Image from "next/image";
-import { useRouter } from "next/router";
-import getWineById from "@/libs/axios/wine/getWineById";
-import { WineData } from "@/types/wines";
 import Button from "@/components/@shared/Button";
+import { WineIdDataProps } from "@/types/wines";
 
-export default function WineDetailCard() {
-  const [data, setData] = useState<WineData | null>(null);
-  const router = useRouter();
-  const { id } = router.query;
-
-  useEffect(() => {
-    const getData = async () => {
-      if (typeof id === "string") {
-        try {
-          const wineData: WineData = await getWineById(id);
-          setData(wineData);
-        } catch (e) {
-          console.error("데이터를 불러오는데 오류가 있습니다:", e);
-        }
-      }
-    };
-
-    getData();
-  }, [id]);
-
-  // 코드 리팩토링 진행 시에 로딩바 애니메이션 추가 예정
-  // 시간이 몇 초 이상 걸리면 에러 페이지로 넘기는 방법도 괜찮을듯
-  if (!data) {
-    return <div>로딩 중...</div>;
-  }
+export default function WineDetailCard({ router, data }: WineIdDataProps) {
 
   const handleBackPage = () => {
-    router.push("/wines");
+    router?.push("/wines");
   }
 
   return (

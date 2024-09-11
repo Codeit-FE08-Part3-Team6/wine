8416defaedad9479446b299f32ce2641d0ad813e
KJongHwa <100202720+KJongHwa@users.noreply.github.com>
2024-09-11 17:55:03 +0900

    Refector : api 1번만 조회하게 수정

--- src/types/wines.ts
+++ src/types/wines.ts
@@ -1,3 +1,5 @@
+import { Router } from "next/router";
+
 export interface WineData {
   id: number;
   name: string;
@@ -52,6 +54,12 @@ export interface WineReviewModalProps {
   value?: number;
   reviewType: string;
   reviewName: string;
+}
+
+export interface WineIdDataProps {
+  router?: Router;
+  data: WineData;
+  reviews?: WineReview[];
 }
 
 export enum WineEnum {

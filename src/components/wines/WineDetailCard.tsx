c46b3369fda1e728290b08ccecc1d594d071137f
KJongHwa <100202720+KJongHwa@users.noreply.github.com>
2024-09-11 17:00:29 +0900

    Feat : 목록페이지 돌아가기 버튼

--- src/components/wines/WineDetailCard.tsx
+++ src/components/wines/WineDetailCard.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import { useRouter } from "next/router";
 import getWineById from "@/libs/axios/wine/getWineById";
 import { WineData } from "@/types/wines";
+import Button from "@/components/@shared/Button";
 
 export default function WineDetailCard() {
   const [data, setData] = useState<WineData | null>(null);
@@ -30,9 +31,13 @@ export default function WineDetailCard() {
     return <div>로딩 중...</div>;
   }
 
+  const handleBackPage = () => {
+    router.push("/wines");
+  }
+
   return (
     <div className="mt-2.5 h-[302px] w-full pt-5 md:mt-5 md:pt-[42px]">
-      <div className="flex h-[260px] w-full rounded-2xl border border-light-gray-300">
+      <div className="relative flex h-[260px] w-full rounded-2xl border border-light-gray-300">
         <div className="flex h-full w-[244px] items-end justify-center">
           <Image
             className="h-4/5"
@@ -54,6 +59,9 @@ export default function WineDetailCard() {
           <div className="mt-2.5 inline-flex h-[37px] items-center rounded-xl bg-light-purple-10 px-2.5 py-1.5 text-md-14px-bold text-light-purple-100 md:mt-5 md:px-[15px] md:py-2 md:text-2lg-18px-bold">
             ₩ {data.price.toLocaleString()}
           </div>
+        </div>
+        <div className="w-10 h-10 absolute top-2 right-2">
+          <Button buttonStyle="light" onClick={handleBackPage}>←</Button>
         </div>
       </div>
     </div>

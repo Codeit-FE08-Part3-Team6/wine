beb6e685fa3a3635c290b0bf645525a39cf2a19e
hyeoksuJ <hyeoksu459@naver.com>
2024-09-12 16:05:35 +0900

    feat: 카드 누르면 와인 상세 페이지 이동

--- src/types/wines.ts
+++ src/types/wines.ts
@@ -91,3 +91,34 @@ export interface WineFilterProps {
   wineRating: number;
   wineName: string;
 }
+
+export interface MyProfileWine {
+  id: number;
+  name: string;
+  region: string;
+  image: string;
+  price: number;
+  type: string;
+  avgRating: number;
+  reviewCount: number;
+  recentReview: {
+    user: {
+      id: number;
+      nickname: string;
+      image: string;
+    };
+    updatedAt: string;
+    createdAt: string;
+    content: string;
+    aroma: string[];
+    rating: number;
+    id: number;
+  };
+  userId: number;
+}
+
+export interface MyProfileWineData {
+  totalCount: number;
+  nextCursor: number;
+  list: MyProfileWine[];
+}

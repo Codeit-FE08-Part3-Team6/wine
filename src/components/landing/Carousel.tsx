af897888cb189a83e2f029b8a26617d52ed3e806
leehj322 <gudwns8956@naver.com>
2024-09-12 23:43:12 +0900

    feat: 캐러샐 슬라이드 터치 이벤트 추가 및 슬라이드 좌우 이동 버튼 추가

--- src/components/landing/Carousel.tsx
+++ src/components/landing/Carousel.tsx
@@ -1,4 +1,11 @@
-import React, { ReactNode, useState, useEffect } from "react";
+import React, {
+  ReactNode,
+  useState,
+  useEffect,
+  TouchEvent,
+  MouseEvent,
+} from "react";
+import Image from "next/image";
 
 interface CarouselProps {
   children: ReactNode;
@@ -7,6 +14,8 @@ interface CarouselProps {
 export default function Carousel({ children }: CarouselProps) {
   const [currentSlide, setCurrentSlide] = useState(0);
   const slidesLength = React.Children.count(children);
+
+  const [touchLocationX, setTouchLocationX] = useState(0);
 
   const changeSlideRight = () => {
     if (currentSlide === slidesLength - 1) {
@@ -34,17 +43,35 @@ export default function Carousel({ children }: CarouselProps) {
     return () => clearInterval(timer);
   }, [currentSlide]);
 
-  const handleRightBtnClick = () => {
+  const handleRightBtnClick = (e: MouseEvent) => {
+    e.preventDefault();
     changeSlideRight();
   };
 
-  const handleLeftBtnClick = () => {
+  const handleLeftBtnClick = (e: MouseEvent) => {
+    e.preventDefault();
     changeSlideLeft();
+  };
+
+  const handleTouchStart = (e: TouchEvent) => {
+    setTouchLocationX(e.changedTouches[0].pageX);
+  };
+
+  const handleTouchEnd = (e: TouchEvent) => {
+    const distanceX = touchLocationX - e.changedTouches[0].pageX;
+
+    if (distanceX >= 80) {
+      changeSlideRight();
+    } else if (distanceX <= -80) {
+      changeSlideLeft();
+    }
   };
 
   return (
     <div className="relative h-full w-full overflow-hidden">
       <div
+        onTouchStart={handleTouchStart}
+        onTouchEnd={handleTouchEnd}
         className="flex h-full transition-transform duration-1000 ease-out [&>*]:flex [&>*]:h-full [&>*]:w-full [&>*]:flex-shrink-0 [&>*]:items-center [&>*]:justify-center"
         style={{ transform: `translateX(-${currentSlide * 100}%)` }}
       >
@@ -52,16 +79,31 @@ export default function Carousel({ children }: CarouselProps) {
       </div>
       <button
         type="button"
-        className="absolute bottom-0 left-0 top-0 w-[50%]"
+        className="absolute bottom-0 left-0 top-0"
         onClick={handleLeftBtnClick}
         aria-label="slide-left-button"
-      />
+      >
+        <Image
+          className="rotate-180"
+          width={18}
+          height={18}
+          src="/images/ic_x_scroll_right.svg"
+          alt="왼쪽 버튼"
+        />
+      </button>
       <button
         type="button"
-        className="absolute bottom-0 right-0 top-0 w-[50%]"
+        className="absolute bottom-0 right-0 top-0"
         onClick={handleRightBtnClick}
         aria-label="slide-right-button"
-      />
+      >
+        <Image
+          width={18}
+          height={18}
+          src="/images/ic_x_scroll_right.svg"
+          alt="왼쪽 버튼"
+        />
+      </button>
       <div className="absolute bottom-0 left-[50%] flex translate-x-[-50%] gap-2">
         {Array.from({ length: 3 }, (_, i) => i).map((idx) => (
           <button
